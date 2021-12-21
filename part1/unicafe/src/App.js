3e63e5562cdac0358dfdea67b1b9a0b619cd9e8e
Chris Williams <christopher.lee.williams@gmail.com>
2021-12-21 09:57:49 -0600

    finishing 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -20,6 +20,8 @@ const Statistics = ({title, good, neutral, bad}) => (
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>average {(good - bad)/(good + neutral + bad)}</p>
+    <p>positive {good/(good + neutral + bad) * 100} %</p>
   </div>
 )
 
