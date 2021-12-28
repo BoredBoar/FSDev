c188871fe7a96e543d732347888db59c949393ff
Chris Williams <christopher.lee.williams@gmail.com>
2021-12-28 10:57:51 -0600

    Completing 2.11

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({handleFilterChange,newValue}) => {
   return (
@@ -45,10 +46,10 @@ const Persons = ({list}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -91,7 +92,16 @@ const App = () => {
     setNewList(list.filter(person => (person.name.toLowerCase().startsWith(val.toLowerCase()))))
   }
 
-  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        updateFilter('',response.data)
+      })
+  }, [])
 
   return (
     <div>
